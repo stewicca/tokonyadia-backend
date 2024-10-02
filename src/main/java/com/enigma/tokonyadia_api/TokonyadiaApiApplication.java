1b965abe2807a4f243666fddd00fc90290a3c176
Steven Wicca Alfredo <stewicalf@gmail.com>
2024-10-02 21:30:32 +0700

    first commit

---
+++ src/main/java/com/enigma/tokonyadia_api/TokonyadiaApiApplication.java
@@ -0,0 +1,11 @@
+package com.enigma.tokonyadia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokonyadiaApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TokonyadiaApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/enigma/tokonyadia_api/TokonyadiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.tokonyadia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokonyadiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
